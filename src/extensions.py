d76e027daadca8e4d334f196c060c1ea06ecbba3
Jekmen1 <jekokobakidze@gmail.com>
2025-01-19 20:06:08 +0400

    impove admin panel

--- src/extensions.py
+++ src/extensions.py
@@ -1,11 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_admin import Admin
+from .admin_views.base import SecureIndexView
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
-admin = Admin()
+admin = Admin(name="App Admin panel", template_mode="bootstrap4",index_view=SecureIndexView())
 
 from .models.user import User
 
