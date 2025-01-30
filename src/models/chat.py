f348d7baf7861ef7c91f561a7948b7b5ba0313c0
Jekmen1 <jekokobakidze@gmail.com>
2025-01-30 22:48:18 +0400

    change room name unique true

--- src/models/chat.py
+++ src/models/chat.py
@@ -5,7 +5,7 @@ class ChatRoom(db.Model):
     __tablename__ = 'chat_rooms'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
 
 class Message(db.Model):
     __tablename__ = 'messages'
